23c37c507262016157339239ca9c76290cca499c
yassin.k3ibat <300136878@monboreal.ca>
2024-10-25 13:37:21 -0400

    Creation de la classe fille PayPal

---
+++ AppPaiements/AppPaiements/Paypal.cs
@@ -0,0 +1,15 @@
+﻿public class PayPal : Paiement
+{
+    public string Courriel { get; set; }
+
+    public PayPal(double montant, string description, string email)
+        : base(montant, description)
+    {
+        Courriel = email;
+    }
+
+    public override void AfficherDetails()
+    {
+        Console.WriteLine($"Paiement par PayPal: {Montant}, Description: {Description}, Email: {Courriel}");
+    }
+}
--- AppPaiements/AppPaiements/Program.cs
+++ AppPaiements/AppPaiements/Program.cs
@@ -4,10 +4,22 @@
     {
         CarteCredit paiement1 = new CarteCredit(50.0, "Achat sur Amazon", 590320);
         CarteCredit paiement2 = new CarteCredit(20.0, "Achat sur eBay", 244920);
-        CarteCredit paiement3 = new CarteCredit(100.0, "Achat sur Fnac", 495930);
+        CarteCredit paiement3 = new CarteCredit(100.0, "Achat sur Uber", 495930);
 
         paiement1.AfficherDetails();
         paiement2.AfficherDetails();
         paiement3.AfficherDetails();
+    
+        Console.WriteLine(Console.ReadLine());
+
+        
+        PayPal paiement4 = new PayPal(150.0, "Paiement de Hotel", "yassin@gmail.com");
+        PayPal paiement5 = new PayPal(75.0, "Achat sur BestBuy", "yassin@gmail.com");
+
+        paiement4.AfficherDetails();
+        paiement5.AfficherDetails();
+        
     }
+
+    
 }

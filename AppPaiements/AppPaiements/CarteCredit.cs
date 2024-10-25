4c8f6e721e5453cd60ce5792f7c3f92568c355ff
yassin.k3ibat <300136878@monboreal.ca>
2024-10-25 12:58:40 -0400

    Creation de la classe fille CarteCredit

---
+++ AppPaiements/AppPaiements/CarteCredit.cs
@@ -0,0 +1,15 @@
+﻿public class CarteCredit : Paiement
+{
+    public int NumeroCarte { get; set; }
+
+    public CarteCredit(double montant, string description, int numeroCarte)
+        : base(montant, description)
+    {
+        NumeroCarte = numeroCarte;
+    }
+
+    public override void AfficherDetails()
+    {
+        Console.WriteLine($"Paiement par Carte de crédit: {Montant}, Description: {Description}, Numéro: {NumeroCarte}");
+    }
+}
--- AppPaiements/AppPaiements/Program.cs
+++ AppPaiements/AppPaiements/Program.cs
@@ -2,6 +2,12 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CarteCredit paiement1 = new CarteCredit(50.0, "Achat sur Amazon", 590320);
+        CarteCredit paiement2 = new CarteCredit(20.0, "Achat sur eBay", 244920);
+        CarteCredit paiement3 = new CarteCredit(100.0, "Achat sur Fnac", 495930);
+
+        paiement1.AfficherDetails();
+        paiement2.AfficherDetails();
+        paiement3.AfficherDetails();
     }
 }
